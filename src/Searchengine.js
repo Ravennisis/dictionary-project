9594da7d1a4d2959e34c0498cf2c47999b1593a2
Ravennisis <76167228+Ravennisis@users.noreply.github.com>
2021-04-09 15:01:24 +0200

    added axios + integrated API + call to API
    https://dictionaryapi.dev/

--- src/Searchengine.js
+++ src/Searchengine.js
@@ -1,13 +1,21 @@
 import React, {useState} from "react";
+import axios from "axios";
 
 import "./Searchengine.css"
 
 export default function Searchengine(){
     let [keyword, setKeyword] = useState("");
+
+    function handleResponse (response){
+console.log(response.data[0]);
+    }
     
     function search (event){
     event.preventDefault();
-    alert (`Searching for definition of ${keyword}`);}
+
+let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+axios.get(apiUrl).then(handleResponse); 
+}
 
 function handleKeywordChange (event) {
     setKeyword(event.target.value);
